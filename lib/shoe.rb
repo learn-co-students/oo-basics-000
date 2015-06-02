3166652e3f9fbeb08085cfb526de84155ed4efb5
gjc2118 <gjc2118@gmail.com>
2015-06-02 00:38:52 -0700

    cobbler

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
+class Shoe
+
+attr_reader :brand
+attr_accessor :color, :size, :material, :condition
+
+def initialize(brand)
+  @brand = brand
+  if BRANDS.find_index(brand) == nil then BRANDS << brand end
+end
+
+def cobble
+  @condition = "new"
+  puts "Your shoe is as good as new!"
+end
+
+BRANDS = []
+
+end
 # Make your shoe class here!
