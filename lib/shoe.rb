4de8a4ffd3a599d68a1a465e1fb92ae03d545659
chaplonglau <chaplonglau@gmail.com>
2015-06-15 16:22:51 -0700

    oo

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
 # Make your shoe class here!
+
+class Shoe
+
+  BRANDS= []
+
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand=brand
+
+    if brand.length>3 
+      if BRANDS.include?(brand)==false
+        BRANDS<<brand
+      end
+    end
+
+  end
+
+  def brand
+    @brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition="new"
+  end
+  
+
+end
