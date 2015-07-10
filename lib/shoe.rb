a4d4fc1c91d46cf437c3f603ef1abc1cbe14728f
garettarrowood <garettarrowood@gmail.com>
2015-07-10 08:08:42 -0400

    Classy

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+require "pry"
+
+class Shoe
+
+	attr_accessor :color, :size, :material, :condition
+	attr_reader :brand
+
+	BRANDS = []
+
+	def initialize(brand)
+		@brand = brand
+		BRANDS<<brand
+		BRANDS.uniq!
+	end
+
+	def cobble
+		puts "Your shoe is as good as new!"
+		@condition = "new"
+	end
+
+end
