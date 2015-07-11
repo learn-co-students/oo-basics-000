f012c2dda0ed26ec063d4078bb48b6b2b7e30cf3
mbrickenworthe <adamwinters456@gmail.com>
2015-07-10 20:43:27 -0400

    Shoe object orientation

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+require "pry"
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+  @brand = brand
+  this = brand
+  if !BRANDS.include?(this)
+  BRANDS << brand
+end
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
