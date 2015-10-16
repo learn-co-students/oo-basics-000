c6a5bd6c216346d45d9ed499c571312371f9c582
NoneOfMaster <rcraigt@gmail.com>
2015-10-16 13:16:13 -0700

    funny shoe pun

---
+++ lib/book.rb
@@ -0,0 +1,20 @@
+class Book
+
+  GENRES = []
+
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
